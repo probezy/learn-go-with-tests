abf52a8648b5cfa295540b974524ffef465fa99f
Michael Zhang <probezy@gmail.com>
2022-05-05 19:49:59 +0800

    update integers

--- integers/add_test.go
+++ integers/add_test.go
@@ -9,6 +9,7 @@ func TestAdder(t *testing.T) {
 	sum := Add(2, 2)
 	expected := 4
 
+	//
 	if sum != expected {
 		t.Errorf("expected '%d', but got '%d'", expected, sum)
 
@@ -20,4 +21,5 @@ func ExampleAdd() {
 	sum := Add(1, 5)
 	fmt.Println(sum)
 	// Output: 6
+
 }
