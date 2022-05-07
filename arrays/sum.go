6b9d207ed67f52001cfdf124e8e04cc1c8dd4fa3
Michael Zhang <probezy@gmail.com>
2022-05-07 16:30:13 +0800

    update array package

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,10 +1,18 @@
 package arrays
 
-func Sum(numbers [5]int) (sum int) {
+func Sum(numbers []int) (sum int) {
 
 	for _, number := range numbers {
 		sum += number
 	}
 
+	return
+}
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sum := Sum(numbers)
+		sums = append(sums, sum)
+	}
 	return
 }
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,12 +1,15 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
-	t.Run("sum is 5 number, sum=15", func(t *testing.T) {
+	t.Run("collection of 5 number", func(t *testing.T) {
 
-		numbers := [5]int{1, 2, 3, 4, 5}
+		numbers := []int{1, 2, 3, 4, 5}
 
 		got := Sum(numbers)
 		want := 15
@@ -18,7 +21,7 @@ func TestSum(t *testing.T) {
 
 	t.Run("sum is 5 number, sum=25", func(t *testing.T) {
 
-		numbers := [5]int{1, 2, 3, 4, 15}
+		numbers := []int{1, 2, 3, 4, 15}
 
 		got := Sum(numbers)
 		want := 25
@@ -27,4 +30,25 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if want != got {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
